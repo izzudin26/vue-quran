03600f59c31bb4cdb6d3e83b9db471c54b340447
izzudin26 <azidarmor26@gmail.com>
2020-07-16 02:33:09 +0700

    deploy

---
+++ precache-manifest.0196bab48461e065e234687d7aaba5b9.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac4429075624b5b4b23d",
+    "url": "/vue-quran/css/app.ffba334a.css"
+  },
+  {
+    "revision": "54cca3eff6367e03b2ed",
+    "url": "/vue-quran/css/chunk-533e2049.76874681.css"
+  },
+  {
+    "revision": "c14900fc868245988d93",
+    "url": "/vue-quran/css/chunk-cc830334.4a438c06.css"
+  },
+  {
+    "revision": "c46c62a02f67bdac8649",
+    "url": "/vue-quran/css/chunk-vendors.1cc28069.css"
+  },
+  {
+    "revision": "93d0644966b4000523beb8b67751201d",
+    "url": "/vue-quran/img/smartphone-quran-min.93d06449.png"
+  },
+  {
+    "revision": "9f704a9307d3d57df8cc0cd5fdd5dd02",
+    "url": "/vue-quran/index.html"
+  },
+  {
+    "revision": "ac4429075624b5b4b23d",
+    "url": "/vue-quran/js/app.3de7c742.js"
+  },
+  {
+    "revision": "54cca3eff6367e03b2ed",
+    "url": "/vue-quran/js/chunk-533e2049.c2923f1c.js"
+  },
+  {
+    "revision": "c14900fc868245988d93",
+    "url": "/vue-quran/js/chunk-cc830334.4fb1831d.js"
+  },
+  {
+    "revision": "c46c62a02f67bdac8649",
+    "url": "/vue-quran/js/chunk-vendors.445be66b.js"
+  },
+  {
+    "revision": "bbc6fe96242bf2ea72381c6a4aa9c86e",
+    "url": "/vue-quran/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-quran/robots.txt"
+  }
+]);
